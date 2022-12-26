2ebd7ce04d9028e5515168b8b2a3d3d12012142c
wei-chun-Nien <116613597+wei-chun-Nien@users.noreply.github.com>
2022-12-26 16:03:09 +0800

    Delete show.js

--- test222/1111-wp-ta-lab-main/demo-diary-app/show.js
+++
@@ -1,9 +0,0 @@
-function start(){
-var button=document.getElementById("start")
-button.addEventListener("click",Myfunction,false);
-}
-
-function Myfunction(){
-let sound=document.getElementById("sounds");
-sound.play();
-}
